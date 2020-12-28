7b947e85d6d883c47b386ff4cafec4dd0abf6413
Michael Schmoock <michael@schmoock.net>
2020-12-28 13:25:46 +0100

    getstats: dynamic timeseries. event and hooks
    getstats: fix hook continue

--- getstats/getstats.py
+++ getstats/getstats.py
@@ -9,14 +9,14 @@ from pyln.client import Plugin, Millisatoshi, RpcError
 from threading import Thread
 
 # TODOs:
+# - thread safety
 # - timeseries: trace success fail rate of forwards per peer
-# - timeseries: fees collected
 # - timeseries: availability per peer
 # - optimize output format for API use
 # - sample result in different OHLC timeframe candles (1hr 1day 1week 1month)
 # - purge db method
 # - limit size, remove old entries
-# - trace certain event hooks as timeseries (e.g. forward events, db writes, ...)
+# - trace event and hook details
 # - reduce data usage by only counting changes and actual events ?
 # - render output method in a nice ASCII chart :D
 
@@ -33,37 +33,63 @@ migrations = [
     "CREATE INDEX idx_data_id ON data (id)",
     "CREATE INDEX idx_data_ts ON data (ts)",
     "CREATE INDEX idx_data_idts ON data (id, ts)",
-    "INSERT INTO timeseries (name) VALUES ('utxo_count')",
-    "INSERT INTO timeseries (name) VALUES ('utxo_amount')",
-    "INSERT INTO timeseries (name) VALUES ('channel_count')",
-    "INSERT INTO timeseries (name) VALUES ('peer_count')",
-    "INSERT INTO timeseries (name) VALUES ('gossiper_count')",
-    "INSERT INTO timeseries (name) VALUES ('liquidity_total_amount')",
-    "INSERT INTO timeseries (name) VALUES ('liquidity_out_amount')",
-    "INSERT INTO timeseries (name) VALUES ('liquidity_in_amount')",
 ]
 
+# Track all variables within a 15min sample
+sample = {
+    # e.g. 'getinfo_num_peers': 0,
+}
 
-def get_tsi(name: str):
-    """ Returns the cached timeseries index """
+
+def check_initialized():
+    if plugin.initialized is False:
+        raise RpcError('getstats', {}, {'message': 'Plugin not yet initialized'})
+
+
+def wait_initialized():
+    while plugin.initialized is False:
+        time.sleep(1)
+
+
+def get_tsi(name: str, create: bool = False):
+    """ get a (cached) timeseries index """
     if name in plugin.tsi_cache:
         return plugin.tsi_cache[name]
     cursor = plugin.db.execute("SELECT id from timeseries WHERE name = ?", (name,))
     row = cursor.fetchone()
     if row is None:
-        raise ValueError("unknown timeseries name")
+        if not create:
+            raise ValueError("unknown timeseries")
+        plugin.db.execute("INSERT INTO timeseries (name) VALUES (?)", (name,))
+        plugin.db.commit()
+        cursor = plugin.db.execute("SELECT id from timeseries WHERE name = ?", (name,))
+        row = cursor.fetchone()
     plugin.tsi_cache[name] = row[0]
     return row[0]
 
 
-def set_data(name: str, value: int, ts: datetime = None):
-    """ Sets an INTEGER datapoint """
+def add_sample(name: str, value: int = None):
+    """ Increments a value within the current sample """
+    if value is None:
+        value = 1
     if type(value) is not int:
         value = int(value)
-    if ts is None:
-        ts = datetime.now()
-    plugin.db.execute("INSERT INTO data (id, ts, value) VALUES (?, ?, ?)",
-                      (get_tsi(name), ts, value))
+    sample[name] = sample.get(name, 0) + value
+
+
+def store_sample():
+    """ stores the current sample values """
+    ts = datetime.now()
+    for name in sample:
+        value = sample[name]
+        plugin.db.execute("INSERT INTO data (id, ts, value) VALUES (?, ?, ?)",
+                          (get_tsi(name, True), ts, value))
+
+
+def reset_sample():
+    """ resets the current sampl values to 0 """
+    for key in sample:
+        sample[key] = 0
 
 
 def get_data(name: str, tsfrom: datetime, tsto: datetime):
@@ -95,6 +121,8 @@ def setup_db(plugin: Plugin):
 
     old_ver = plugin.db.execute("SELECT max(id) FROM migrations").fetchone()[0]
     old_ver = old_ver if old_ver is not None else 0
+    if old_ver > len(migrations):
+        raise Exception('Database has newer state than expected')
 
     # apply migrations ...
     i = 0
@@ -121,7 +149,7 @@ def job(plugin: Plugin):
     plugin.count += 1
 
     # partly taken from summary.py
-    # info = plugin.rpc.getinfo()
+    info = plugin.rpc.getinfo()
     funds = plugin.rpc.listfunds()
     peers = plugin.rpc.listpeers()
     utxos = [int(f['amount_msat']) for f in funds['outputs'] if f['status'] == 'confirmed']
@@ -135,6 +163,7 @@ def job(plugin: Plugin):
         for c in p['channels']:
             if c['state'] != 'CHANNELD_NORMAL':
                 continue
+            num_channels += 1
             active_channel = True
             if p['connected']:
                 num_connected += 1
@@ -149,34 +178,56 @@ def job(plugin: Plugin):
             else:
                 to_them = Millisatoshi(0)
             avail_in += to_them
-            num_channels += 1
         if not active_channel and p['connected']:
             num_gossipers += 1
 
-    set_data('utxo_count', len(utxos))
-    set_data('utxo_amount', Millisatoshi(sum(utxos)))
-    set_data('channel_count', num_channels)
-    set_data('peer_count', num_connected)
-    set_data('gossiper_count', num_gossipers)
-    set_data('liquidity_total_amount', avail_out + avail_in)
-    set_data('liquidity_out_amount', avail_out)
-    set_data('liquidity_in_amount', avail_in)
+    add_sample('getinfo_num_peers', info['num_peers'])
+    add_sample('getinfo_num_pending_channels', info['num_pending_channels'])
+    add_sample('getinfo_num_active_channels', info['num_active_channels'])
+    add_sample('getinfo_num_inactive_channels', info['num_inactive_channels'])
+    add_sample('getinfo_fees_collected_msat', info['fees_collected_msat'])
+
+    add_sample('summary_num_utxos', len(utxos))
+    add_sample('summary_utxo_amount', Millisatoshi(sum(utxos)))
+    add_sample('summary_num_channels', num_channels)
+    add_sample('summary_num_connected', num_connected)
+    add_sample('summary_num_gossipers', num_gossipers)
+    add_sample('summary_avail_total', avail_out + avail_in)
+    add_sample('summary_avail_out', avail_out)
+    add_sample('summary_avail_in', avail_in)
+
+    store_sample()
+    reset_sample()
     plugin.db.commit()
 
 
 def scheduler(plugin: Plugin):
-    """ Simply calls the scheduler again and again """
-    schedule.every().hour.at(":37").do(job, plugin)
-    # schedule.every().second.do(job, plugin)
-    while 1:
+    """ Simply calls the schedule job at given interval """
+    wait_initialized()
+    # call once initially to make testing easier
+    job(plugin)
+    # reschedule at given interval
+    schedule.every(15).minutes.do(job, plugin)
+    while True:
         schedule.run_pending()
-        time.sleep(10)
-        # time.sleep(0.1)
+        time.sleep(1)
 
 
-def check_initialized():
-    if plugin.initialized is False:
-        raise RpcError('getstats', {}, {'message': 'Plugin not yet initialized'})
+@plugin.subscribe("forward_event")
+def forward_event(plugin: Plugin, forward_event: dict, **kwargs):
+    add_sample('forward_event')
+
+
+@plugin.hook('db_write')
+def on_db_write(writes, data_version, plugin, **kwargs):
+    add_sample('db_write')
+    return {"result": "continue"}
+
+
+@plugin.hook('htlc_accepted')
+def on_htlc_accepted(onion, htlc, plugin, **kwargs):
+    add_sample('htlc_accepted')
+    return {"result": "continue"}
 
 
 @plugin.method('getstats')
